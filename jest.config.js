1844d70432b33e492000625bc9f429c547e1964e
pozil <philippe.ozil@gmail.com>
2019-09-11 00:29:32 +0200

    Initial commmit

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
-const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
+// You only need this file
+// - if you don't want to customize your Jest environment
+// - if you don't want to use Jest i. e. from a Visual Studio Code extension
+const { jestConfig } = require('lwc-services/lib/config/jestConfig');
+
 module.exports = {
     ...jestConfig
+    // Add your custom Jest configuration
 };
