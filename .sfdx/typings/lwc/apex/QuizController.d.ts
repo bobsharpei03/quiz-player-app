a28e3b159616bc3fbb6ce2d771efcd411d90e091
Anny He <anny.he@salesforce.com>
2019-10-29 09:53:24 -0700

    Added bar chart that displays audience answers

--- .sfdx/typings/lwc/apex/QuizController.d.ts
+++ .sfdx/typings/lwc/apex/QuizController.d.ts
@@ -4,6 +4,9 @@ declare module "@salesforce/apex/QuizController.getQuizSettings" {
 declare module "@salesforce/apex/QuizController.getCurrentQuestion" {
   export default function getCurrentQuestion(param: {sessionId: any}): Promise<any>;
 }
+declare module "@salesforce/apex/QuizController.getAnswerMap" {
+  export default function getAnswerMap(): Promise<any>;
+}
 declare module "@salesforce/apex/QuizController.getPlayersSortedByScore" {
   export default function getPlayersSortedByScore(param: {maxFetchCount: any}): Promise<any>;
 }
