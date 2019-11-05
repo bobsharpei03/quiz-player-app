5d6fa23c948133a019937e550af0fc8bdf4c54b2
Philippe Ozil <pozil@salesforce.com>
2019-11-05 23:25:24 +0100

    Answer stats fix (#38)
    * Ignored jsconfig.json
    
    * Got rid of Aura linting
    
    * Now retrieving data in answer chart component
    
    * Prettier

--- .sfdx/typings/lwc/apex/QuizController.d.ts
+++ .sfdx/typings/lwc/apex/QuizController.d.ts
@@ -1,11 +1,17 @@
 declare module "@salesforce/apex/QuizController.getQuizSettings" {
   export default function getQuizSettings(): Promise<any>;
 }
+declare module "@salesforce/apex/QuizController.getWinnerStats" {
+  export default function getWinnerStats(): Promise<any>;
+}
+declare module "@salesforce/apex/QuizController.getPlayerAnswerStats" {
+  export default function getPlayerAnswerStats(param: {playerId: any}): Promise<any>;
+}
 declare module "@salesforce/apex/QuizController.getCurrentQuestion" {
   export default function getCurrentQuestion(param: {sessionId: any}): Promise<any>;
 }
-declare module "@salesforce/apex/QuizController.getAnswerMap" {
-  export default function getAnswerMap(): Promise<any>;
+declare module "@salesforce/apex/QuizController.getAnswerStats" {
+  export default function getAnswerStats(): Promise<any>;
 }
 declare module "@salesforce/apex/QuizController.getPlayersSortedByScore" {
   export default function getPlayersSortedByScore(param: {maxFetchCount: any}): Promise<any>;
